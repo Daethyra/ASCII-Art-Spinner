b89134f9f97a0e316f60781641f97ca5303bc87a
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-03-16 19:38:05 -0700

    Add files via upload

---
+++ sample_caller.py
@@ -0,0 +1,7 @@
+import time
+from spinning_globe import start_animation
+
+start_animation()
+
+# Your other code here
+time.sleep(10)  # Example: sleep for 10 seconds while the animation is playing
